c278d17bc0028846d32788ed902d331c73564921
spuggle <spuggle@protonmail.com>
2021-08-09 00:59:18 +0500

    feat: integrate REST API via express

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,19 @@
 import * as dotenv from "dotenv";
+import * as express from "express";
 import { SneakerClient } from "./structs/SneakerClient";
 import { messageCreate } from "./listeners/client/messageCreate";
 
 dotenv.config();
+
+const app = express();
+app.get("/", (_, response) => {
+  response.send("OK");
+});
 
 const client = new SneakerClient();
 client.on("messageCreate", messageCreate);
 
 void client.login(process.env.BOT_TOKEN)
   .then(() => console.log("Successfully logged in!"));
+
+void app.listen(process.env.PORT || 80);
